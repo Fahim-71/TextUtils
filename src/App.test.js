2b8b8fdff67da73ffa72add44bed7f8dbdb4b423
Fahim-71 <fahimn573@gmail.com>
2026-08-26 13:46:16 -0700

    done

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the TextUtils navigation', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('TextUtils')).toBeInTheDocument();
+  expect(screen.getByText('About TextUtils')).toBeInTheDocument();
 });

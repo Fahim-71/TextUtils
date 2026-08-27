50f07048d3651d5c3c2e7ad5214b876eefeafadb
Fahim-71 <fahimn573@gmail.com>
2026-08-27 03:59:14 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
-//import TextForm from './components/TextForm'
-import About from './components/About'
+import TextForm from './components/TextForm'
+//import About from './components/About'
 function App() {
   return (
     <>
@@ -9,8 +9,8 @@ function App() {
 <Navbar title="TextUtils"/>
 <div className="container my-4">
 
- {/*<TextForm heading="Enter the text to analyze"/>*/} 
- <About/>
+ <TextForm heading="Enter the text to analyze"/>
+ {/*<About/>*/}
 </div>
 
     

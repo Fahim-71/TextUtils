2862bfb1698d97b12bd43935606be1d05e121ce0
Fahim-71 <fahimn573@gmail.com>
2026-08-28 00:01:52 -0700

    done

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -19,6 +19,18 @@ export default function TextForm(props) {
     //console.log("On change")
     setText(event.target.value)
   }
+
+  const handleCopy = () =>{
+    console.log("I am copy")
+    var text = document.getElementById("myBox")
+    text.select()
+    navigator.clipboard.writeText(text.value)
+  }
+
+  const handleExtraSpaces = () =>{
+    let newText = text.split(/[ ]+/)
+    setText(newText.join(" "))
+  }
     const [text, setText] = useState('Enter text here')
     //setText("new text")
   return (
@@ -31,6 +43,8 @@ export default function TextForm(props) {
 </div>
   <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
   <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+  <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+  <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
     </div>
     <div className="container my-3">
       <h1>Your text summary</h1>
